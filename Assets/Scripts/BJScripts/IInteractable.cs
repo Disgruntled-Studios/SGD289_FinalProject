0fb590c4245d0cbd5d8eba84f3e5fe89cefdf089
Brian <brobertjernigan@gmail.com>
2025-05-30 10:50:54 -0400

    IInteractable

--- Assets/Scripts/BJScripts/IInteractable.cs
+++ Assets/Scripts/BJScripts/IInteractable.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+// Interface for interactable objects
+// Have any interactable object script inherit from this
+// See: MirrorController
+
 public interface IInteractable
 {
     void Interact(Transform player);
