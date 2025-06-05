8e4eb302312bcdefc467d7d65455497b55510130
Brian <brobertjernigan@gmail.com>
2025-06-04 20:34:54 -0400

    FPS GUn

--- Assets/Scripts/BJScripts/FPSGunController.cs
+++ Assets/Scripts/BJScripts/FPSGunController.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 
 public class FPSGunController : MonoBehaviour
 {
+    [SerializeField] private GameObject _gunModel;
+    [SerializeField] private LineRenderer _lr;
+
+    private bool _isAiming;
+
+    private void Update()
+    {
+        _gunModel.SetActive(_isAiming);
+        _lr.enabled = _isAiming;
+    }
+    
     public void StartGunAim()
     {
-        Debug.Log("Aiming");
-        return;
+        _isAiming = true;
     }
 
     public void EndGunAim()
     {
-        Debug.Log("Not aiming anymore");
-        return;
+        _isAiming = false;
     }
 
     public void Shoot()
