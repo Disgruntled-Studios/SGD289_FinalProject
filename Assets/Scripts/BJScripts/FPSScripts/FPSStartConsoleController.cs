a9546157bebb115e07cb45988555fe8ed5390160
Brian <brobertjernigan@gmail.com>
2025-06-11 20:05:30 -0400

    Deleted IGunController

---
+++ Assets/Scripts/BJScripts/FPSScripts/FPSStartConsoleController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FPSStartConsoleController : MonoBehaviour, IInteractable
+{
+    public void Interact(Transform player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
