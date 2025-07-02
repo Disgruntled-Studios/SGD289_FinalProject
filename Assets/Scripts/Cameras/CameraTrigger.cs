d0bc831dd71adfefaaf5a9127ec4eb4b6feef53b
Brian <brobertjernigan@gmail.com>
2025-07-02 16:42:49 -0400

    Cameras

--- Assets/Scripts/Cameras/CameraTrigger.cs
+++ Assets/Scripts/Cameras/CameraTrigger.cs
@@ -1,23 +1,26 @@
+using System;
 using TMPro;
 using UnityEngine;
 using Unity.Cinemachine;
 using UnityEngine.Serialization;
 
-
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 public class CameraTrigger : MonoBehaviour
 {
     [SerializeField] private string _camId;
-
+    
     void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out PlayerController pc))
         {
-            CameraManager.Instance.TrySwitchToCamera(_camId);
-            CameraManager.Instance.TrySetCameraTarget(_camId, GameManager.Instance.CameraTarget);
+            CameraManager.Instance.RegisterActiveTrigger(_camId);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerController pc))
+        {
+            CameraManager.Instance.UnregisterActiveTrigger(_camId);
         }
     }
 }
