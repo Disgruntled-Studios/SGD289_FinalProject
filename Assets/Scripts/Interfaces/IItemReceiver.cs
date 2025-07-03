55271e6bc6a531707b3a7c9954ae4c70549e531e
Brian <brobertjernigan@gmail.com>
2025-07-03 00:36:59 -0400

    Receiver logic fix

--- Assets/Scripts/Interfaces/IItemReceiver.cs
+++ Assets/Scripts/Interfaces/IItemReceiver.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 public interface IItemReceiver
 {
     string Name { get; }
+    bool ItemHasBeenReceived { get; set; }
     bool TryReceiveItem(PlayerInventory inventory, InventoryItem item);
 }
