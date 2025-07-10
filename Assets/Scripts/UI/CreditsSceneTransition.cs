f507f3035459a8d655350edb413b51aee5284797
Kristen <143353685+ArtGirl3@users.noreply.github.com>
2025-07-10 02:26:26 -0400

    Added some code so credits screen will go to end screen after a short time
    Needs testing though, concerned that video sometimes skips Andrew's part of the video and seems to have a short pause before transitioning to main menu after video ends.

--- Assets/Scripts/UI/CreditsSceneTransition.cs
+++ Assets/Scripts/UI/CreditsSceneTransition.cs
@@ -1,8 +1,40 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class CreditsSceneTransition : MonoBehaviour
 {
+    [SerializeField]
+    private bool usingCreditVideo = true;
+    [SerializeField]
+    private float waitTime = 39f;
+    private float baseTime = 0f; //holds current time
+    private float targetTime; //holds target time
+    private float resetTime = 0f; //any delay before credit video starts such as for a cinematic or win screen.
+
+
+    void Start()
+    {
+        if(usingCreditVideo == true)
+        {
+            baseTime = (baseTime + Time.deltaTime) - resetTime;
+            targetTime = baseTime + waitTime;
+        }
+    }
+
+    //Added code to trigger end credits after a specified time if using video instead of an animation.
+    void Update()
+    {
+        if (usingCreditVideo == true)
+        {
+            baseTime = (baseTime + Time.deltaTime) - resetTime;
+            if (baseTime > targetTime)
+            {
+                EndCredits();
+                print("returning to main menu");
+            }
+        }
+    }
     public void EndCredits()
     {
         SceneManager.LoadScene(0);
