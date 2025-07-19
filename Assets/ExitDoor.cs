054d146f765da24a1a8412581a70a32c4ea62d6c
Brian <brobertjernigan@gmail.com>
2025-07-19 16:40:06 -0400

    Ending Sequence

--- Assets/ExitDoor.cs
+++ Assets/ExitDoor.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitDoor : MonoBehaviour, IInteractable
 {
@@ -6,16 +8,35 @@ public class ExitDoor : MonoBehaviour, IInteractable
     [SerializeField] private string _targetSceneName;
     [SerializeField] private string _targetCameraId;
 
-    private bool _isItem = false;
-    public bool IsItem => _isItem;
+    [SerializeField] private Material _glitchDoorMat;
+    [SerializeField] private Material _glitchKnobMat;
+    [SerializeField] private MeshRenderer _meshRenderer;
 
+    [SerializeField] private GameObject _glitchAudio;
+    
     public void Interact(Transform player, PlayerInventory inventory)
     {
-        if (!string.IsNullOrEmpty(_targetSceneName))
+        if (string.IsNullOrEmpty(_targetSceneName)) return;
+
+        if (_meshRenderer != null && _glitchDoorMat != null && _glitchKnobMat != null)
         {
-            TransitionManager.Instance.GetComponent<Animator>().SetTrigger("StartFadeOut");
-            GameManager.Instance.PlayerController.enabled = false;
+            var newMats = _meshRenderer.materials;
+            if (newMats.Length >= 2)
+            {
+                newMats[0] = _glitchDoorMat;
+                newMats[1] = _glitchKnobMat;
+                _meshRenderer.materials = newMats;
+            }
         }
+
+        if (_glitchAudio)
+        {
+            _glitchAudio.SetActive(true);
+        }
+        
+        GameManager.Instance.PlayerController.enabled = false;
+
+        StartCoroutine(FadeAndLoadScene());
     }
 
     public void OnEnter()
@@ -26,5 +47,13 @@ public class ExitDoor : MonoBehaviour, IInteractable
     public void OnExit()
     {
         _interactionPrompt.SetActive(false);
+    }
+
+    private IEnumerator FadeAndLoadScene()
+    {
+        yield return UIManager.Instance.StartCoroutine(UIManager.Instance.Fade(0f, 1f, 2.5f, () =>
+        {
+            SceneManager.LoadScene(_targetSceneName);
+        }));
     }
 }
